a090cc15f9d7943af35af0b9159a787156d70abe
yurimarim <yurimarim@hotmail.com>
2025-02-09 18:21:27 -0300

    fix: change Main component to Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { ThemeProvider } from "styled-components";
-import { Main } from "./components/Main";
 import defaultTheme from "./assets/styles/themes/default";
+import { Home } from "./components/Home";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Main />
+      <Home />
     </ThemeProvider>
   );
 }
